05dbab98b5283233331e7ca3c28069daceec2ebc
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-07-26 19:35:49 +0530

    routes added

---
+++ server/DBConnection.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("database connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = connectDB;
---
+++ server/index.js
@@ -0,0 +1,30 @@
+//
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const DBconnect = require("./DBConnection");
+const app = express();
+const authRoutes = require("./routes/authRoutes");
+
+const { errorMiddleware } = require("./middlewares/ErrorHandler");
+app.use(express.json());
+app.use(cors({ origin: true }));
+app.use(express.urlencoded({ extended: false }));
+const server = app.listen(process.env.PORT);
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URL);
+//     console.log("database connected");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+DBconnect();
+// connectDB();
+console.log(`Connected to port ${process.env.PORT}`);
+
+// Global Error Handling
+app.use(errorMiddleware);
+
+//Routes
+app.use("/api", authRoutes, errorMiddleware);
