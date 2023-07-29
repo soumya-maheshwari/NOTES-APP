f35c5df0d057fd075ec1e46490dcfddafdc3973d
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-07-29 13:32:44 +0530

    changes for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 //
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const cors = require("cors");
 const DBconnect = require("./DBConnection");
 const app = express();
@@ -12,21 +13,21 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 app.use(express.urlencoded({ extended: false }));
 const server = app.listen(process.env.PORT);
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URL);
-//     console.log("database connected");
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-DBconnect();
-// connectDB();
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("database connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+// DBconnect();
+connectDB();
 console.log(`Connected to port ${process.env.PORT}`);
 
 // Global Error Handling
 app.use(errorMiddleware);
 
 //Routes
-app.use("/api", authRoutes, errorMiddleware);
-app.use("/api/notes", noteRoutes, errorMiddleware);
+app.use(authRoutes, errorMiddleware);
+app.use("/notes", noteRoutes, errorMiddleware);
