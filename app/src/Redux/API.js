9c73ad6ba86a5c6e7fca8d9c686eeacfb182add3
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-07-29 13:36:03 +0530

    created Readme

--- app/src/Redux/API.js
+++ app/src/Redux/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/",
-  // baseURL: "https://notesapp-c9ic.onrender.com/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://notesapp-c9ic.onrender.com/",
 });
